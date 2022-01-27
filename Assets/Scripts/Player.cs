611057da68a2336c3dd50189e3e7efa2fddf4c20
AidanHarris001 <57464127+AidanHarris001@users.noreply.github.com>
2022-01-27 16:02:03 +0000

    Update Player.cs
    Added:
    thrustSpeed
    rigid body handling
    thrust detection
    turn detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float thrustSpeed = 1.0f;
+    private Rigidbody2D _rigidbody;
+    private bool _thursting;
+    private float _turnDirection;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,21 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _thursting = Input.GetKey(KeyCode.UpArrow);
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            _turnDirection = 1.0f;
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            _turnDirection = -1.0f;
+        } else {
+            _turnDirection = 0.0f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_thursting) {
+            _rigidbody.AddForce(this.transform.up * this.thrustSpeed);
+        }
     }
 }
