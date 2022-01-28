c2415511ef5138b2e485548308e47c14667d4e06
AidanHarris001 <57464127+AidanHarris001@users.noreply.github.com>
2022-01-28 14:48:27 +0000

    Update GameManager.cs
    Players are invincible for 3 seconds after respawning to prevent asteriods spawn killing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ public class GameManager : MonoBehaviour
     public Player player;
     public float respawnTime = 3.0f;
     public int lives = 3;
+    public float invincibleTime = 3.0f;
 
     public void PlayerDied()
     {
@@ -20,7 +21,15 @@ public class GameManager : MonoBehaviour
     private void Respawn()
     {
         this.player.transform.position = Vector3.zero;
+        this.player.gameObject.layer = LayerMask.NameToLayer("IgnoreCollisions");
         this.player.gameObject.SetActive(true);
+        
+        Invoke(nameof(TurnOnCollisions), this.invincibleTime);
+    }
+
+    private void TurnOnCollisions()
+    {
+        this.player.gameObject.layer = LayerMask.NameToLayer("Player");
     }
 
     private void GameOver()
