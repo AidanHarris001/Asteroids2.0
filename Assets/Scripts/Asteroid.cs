91344c22518459d442229063ec07851f0b0d0c05
AidanHarris001 <57464127+AidanHarris001@users.noreply.github.com>
2022-01-28 10:49:18 +0000

    Asteroids spawn into scene
    at random angles and with random rotations

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -6,9 +6,11 @@ public class Asteroid : MonoBehaviour
     public float size = 1.0f;
     public float minSize = 0.5f;
     public float maxSize = 1.5f;
+    public float speed = 50.0f;
+    public float maxLifeTime = 10.0f;
 
     private SpriteRenderer _spriteRenderer;
-    private Rigidbody _rigidbody;
+    private Rigidbody2D _rigidbody;
 
     private void Awake()
     {
@@ -26,6 +28,13 @@ public class Asteroid : MonoBehaviour
         //controls the size of the spawned asteriod
         this.transform.localScale = Vector3.one * this.size;
 
-        _rigidbody.mass = this.size;
+        _rigidbody.mass = this.size * 2.0f;
+    }
+
+    public void SetTrajectory(Vector2 direction)
+    {
+        _rigidbody.AddForce(direction * this.speed);
+
+        Destroy(this.gameObject, this.maxLifeTime);
     }
 }
